0be0b446b3933ec281afec3bdf0a77f41d30ac3b
Dayansito <dayansito120@hotmail.com>
2025-10-16 19:00:21 -0400

    Update player-props.js

--- api/player-props.js
+++ api/player-props.js
@@ -5,7 +5,8 @@ export default async function handler(req, res) {
     
     const { eventId } = req.query;
     const sport = 'americanfootball_nfl';  // ← MUST BE EXACT
-    const markets = 'h2h,spreads,totals,player_pass_tds,player_pass_yds,player_rush_yds,player_reception_yds,player_anytime_td,player_first_td';
+    const markets = 'h2h,spreads,totals,player_pass_tds,player_pass_yds,player_pass_completions,player_pass_attempts,player_rush_yds,player_rush_attempts,player_receptions,player_reception_yds,player_anytime_td,player_last_td,player_kicking_points';
+```
     
     const url = `https://api.the-odds-api.com/v4/sports/${sport}/events/${eventId}/odds/?apiKey=${API_KEY}&regions=us,us2&markets=${markets}&oddsFormat=american`;
 
