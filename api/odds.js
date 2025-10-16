b751743eeeabaa2b11d9b26e21e9d3616d2aa921
Dayansito <dayansito120@hotmail.com>
2025-10-16 01:22:43 -0400

    Update odds.js

--- api/odds.js
+++ api/odds.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const url = `https://api.the-odds-api.com/v4/sports/${sport}/odds/?apiKey=${API_KEY}&regions=us&markets=h2h&oddsFormat=american`;
+    const url = `https://api.the-odds-api.com/v4/sports/${sport}/odds/?apiKey=${API_KEY}&regions=us,us2&markets=h2h&oddsFormat=american`;
     const response = await fetch(url);
     const data = await response.json();
     return res.status(200).json(data);
